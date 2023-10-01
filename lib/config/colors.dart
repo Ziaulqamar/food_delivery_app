d6c165feb921a2eea0e0c00fbb8e3cdcdd694b4f
Muhammad Zia Ul Qamar <devzia@Mahars-Macbook.local>
2023-10-01 21:35:24 +0500

    Trying to build Appbar

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const gradientGreen1 = Color(0xff53E88B);
   static const gradientGreen2 = Color(0xff15BE77);
-  static final dropshadowColor = const Color(0xff5A6CEA).withOpacity(0.07);
+  static final dropshadowColor = const Color(0xff5A6CEA).withOpacity(0.1);
 
   static const customOrange = Color(0xffF9A84D);
   static const customTextOrange = Color(0xffFF7C32);
--- lib/screens/home_screen/homeScreen.dart
+++ lib/screens/home_screen/homeScreen.dart
@@ -24,89 +24,141 @@ class Homescreen extends StatelessWidget {
       stretched: false,
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
+        child: Stack(
           children: [
-            SizedBox(height: 40.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Column(
               children: [
-                Text(
-                  'Find Your\nFavourite Food',
-                  style: TextStyle(
-                    fontSize: 31.sp,
-                    fontFamily: 'BentonSans Bold',
-                  ),
-                ),
-                const NotificationButton(),
-              ],
-            ),
-            SizedBox(height: 20.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                HomeScreenCustomTextField(),
-                CustomFilterButton(),
-              ],
-            ),
-            SizedBox(height: 20.h),
-            SizedBox(
-              height: 425.h,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                physics: const BouncingScrollPhysics(),
-                child: Column(
+                SizedBox(height: 40.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    PromotionalCardWidget(),
-                    SizedBox(height: 20.h),
-                    Column(
+                    Text(
+                      'Find Your\nFavourite Food',
+                      style: TextStyle(
+                        fontSize: 31.sp,
+                        fontFamily: 'BentonSans Bold',
+                      ),
+                    ),
+                    const NotificationButton(),
+                  ],
+                ),
+                SizedBox(height: 20.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    HomeScreenCustomTextField(),
+                    CustomFilterButton(),
+                  ],
+                ),
+                SizedBox(height: 20.h),
+                SizedBox(
+                  height: 477.h,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    physics: const BouncingScrollPhysics(),
+                    child: Column(
                       children: [
-                        restaurantsWidget(title: 'Nearest Restaurants'),
+                        PromotionalCardWidget(),
                         SizedBox(height: 20.h),
-                        Container(
-                          height: 184.h,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: AppColors.dropshadowColor,
-                                blurRadius: 50.r,
-                                offset: const Offset(0, 0),
+                        Column(
+                          children: [
+                            restaurantsWidget(title: 'Nearest Restaurants'),
+                            SizedBox(height: 20.h),
+                            Container(
+                              height: 184.h,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: AppColors.dropshadowColor,
+                                    blurRadius: 50.r,
+                                    offset: const Offset(0, 0),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            physics: const BouncingScrollPhysics(),
-                            children: [
-                              RestaurantCard(
-                                image: 'assets/images/veganRestaurant.png',
-                                restaurantName: 'Vegan Resto',
-                                restaurantTime: '12 Mins',
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                physics: const BouncingScrollPhysics(),
+                                children: [
+                                  RestaurantCard(
+                                    image: 'assets/images/veganRestaurant.png',
+                                    restaurantName: 'Vegan Resto',
+                                    restaurantTime: '12 Mins',
+                                  ),
+                                  RestaurantCard(
+                                    image:
+                                        'assets/images/healthyRestaurant.png',
+                                    restaurantName: 'Healthy Food',
+                                    restaurantTime: '8 Mins',
+                                  ),
+                                  RestaurantCard(
+                                    image:
+                                        'assets/images/goodFoodRestaurant.png',
+                                    restaurantName: 'Good Food',
+                                    restaurantTime: '12 Mins',
+                                  ),
+                                ],
                               ),
-                              RestaurantCard(
-                                image: 'assets/images/healthyRestaurant.png',
-                                restaurantName: 'Healthy Food',
-                                restaurantTime: '8 Mins',
-                              ),
-                              RestaurantCard(
-                                image: 'assets/images/goodFoodRestaurant.png',
-                                restaurantName: 'Good Food',
-                                restaurantTime: '12 Mins',
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 20.h),
-                        restaurantsWidget(title: 'Popular Menu'),
-                        SizedBox(height: 20.h),
-                        PopularMenuItem(
-                          image: 'assets/images/greenNoodles.png',
-                          title: 'Green Noodles',
-                          subtitle: 'Noodle Home',
-                          price: '15',
+                            ),
+                            SizedBox(height: 20.h),
+                            restaurantsWidget(title: 'Popular Menu'),
+                            SizedBox(height: 20.h),
+                            PopularMenuItem(
+                              image: 'assets/images/greenNoodles.png',
+                              title: 'Green Noodles',
+                              subtitle: 'Noodle Home',
+                              price: '15',
+                            ),
+                          ],
                         ),
                       ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 10.h),
+                width: double.infinity,
+                height: 74.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.dropshadowColor,
+                      blurRadius: 50.r,
+                      offset: const Offset(0, 0),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 44.h,
+                      width: 105.w,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            AppColors.gradientGreen1.withOpacity(0.1),
+                            AppColors.gradientGreen2.withOpacity(0.1),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(12.r),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset('assets/icons/Iconly/Bulk/Home.svg'),
+                          Text('Home'),
+                        ],
+                      ),
                     ),
                   ],
                 ),
